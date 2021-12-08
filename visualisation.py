037ed94db534907d822c670e7fc4107b375f7d47
amberremmel <54839039+amberremmel@users.noreply.github.com>
2021-12-08 15:55:43 +0100

    Add files via upload

--- visualisation.py
+++ visualisation.py
@@ -15,70 +15,77 @@ import ast
 xaxis = []
 yaxis = []
 
-t_end = 50
-n = 5
+t_end = 15
+n = 15
+z = 0.0134
+
+au = 1.5e13 # cm
+RSun = 696340e5 #cm
+
+# with open("t_end={}yr_n={}_z={}/time_for_loop_t_end={}yr_n={}_z={}.csv".format(t_end, n, z, t_end, n, z)) as csvfile:
+#     time_file = csv.reader(csvfile)
+#     temperatures = ast.literal_eval(next(time_file)[1])
+
+times = np.arange(0, 15*n, 0.05)
+label_names = ["core", "gas", "earth"]
+
+# The csv file is otherwise too big to use
+csv.field_size_limit(sys.maxsize)
+
+# Make two dictionaries to save the coordinates in
+x_coordinates = dict()
+y_coordinates = dict()
+
+with open("t_end={}yr_n={}_z={}/coordinates_t_end={}yr_n={}_z={}.csv".format(t_end, n, z, t_end, n, z)) as csvfile:
+    coordinates = csv.reader(csvfile)
+
+    # csv file: row[0]=name, row[1]=x, row[2]=y
+    for row in coordinates:
+        # The ast.literal_eval makes from a string list a list with floats
+        x_coordinates[row[0]] = ast.literal_eval(row[1])
+        y_coordinates[row[0]] = ast.literal_eval(row[2])
+
+result = plt.scatter(np.array(x_coordinates["earth"])/RSun, np.array(y_coordinates["earth"])/RSun, label="earth", c=times, s=2)
 
 for i in range(n):
-    gravity_particles = read_set_from_file("good_files/gravity_particles_t_end={}yr_n={}_i={}.amuse".format(t_end, n, i), "amuse", append_to_file=False)
-    hydro_particles = read_set_from_file("good_files/hydro_particles_t_end={}yr_n={}_i={}.amuse".format(t_end, n, i), "amuse", append_to_file=False)
-
-    # print("Number of gravity particles =", len(gravity_particles))
-    # print("Number of hydro particles (including the core particle) =", len(hydro_particles))
-
-    # # The csv file is otherwise too big to use
-    # csv.field_size_limit(sys.maxsize)
-
-    # # Make two dictionaries to save the coordinates in
-    # x_coordinates = dict()
-    # y_coordinates = dict()
-
-    # with open("coordinates_t_end=100yr_n=5.csv") as csvfile:
-    #     coordinates = csv.reader(csvfile)
-
-    #     # csv file: row[0]=name, row[1]=x, row[2]=y
-    #     for row in coordinates:
-    #         # The ast.literal_eval makes from a string list a list with floats
-    #         x_coordinates[row[0]] = ast.literal_eval(row[1])
-    #         y_coordinates[row[0]] = ast.literal_eval(row[2])
-
-    au = 1.5e13 # cm
-    RSun = 696340e5 #cm
-
-    # Core, gas, earth, moon
-    # labels = list(x_coordinates.keys())
-    labels = ["core", "gas", "earth", "moon"]
+    gravity_particles = read_set_from_file("t_end={}yr_n={}_z={}/gravity_particles_t_end={}yr_n={}_i={}_z={}.amuse".format(t_end, n, z, t_end, n, i, z), "amuse", append_to_file=False)
+    hydro_particles = read_set_from_file("t_end={}yr_n={}_z={}/hydro_particles_t_end={}yr_n={}_i={}_z={}.amuse".format(t_end, n, z, t_end, n, i, z), "amuse", append_to_file=False)
 
     # Coordinates at the end of the run
-    plt.scatter(hydro_particles.x[1:].value_in(units.cm)/RSun, hydro_particles.y[1:].value_in(units.cm)/RSun, label="gas")
-    plt.scatter(hydro_particles.x[0].value_in(units.cm)/RSun, hydro_particles.y[0].value_in(units.cm)/RSun, label=labels[0])
-    plt.scatter(gravity_particles.x[0].value_in(units.cm)/RSun, gravity_particles.y[0].value_in(units.cm)/RSun, label=labels[2])
-    plt.scatter(gravity_particles.x[1].value_in(units.cm)/RSun, gravity_particles.y[1].value_in(units.cm)/RSun, label=labels[3])
+    core = plt.scatter(hydro_particles.x[1:].value_in(units.cm)/RSun, hydro_particles.y[1:].value_in(units.cm)/RSun, color="blue")
+    gas = plt.scatter(hydro_particles.x[0].value_in(units.cm)/RSun, hydro_particles.y[0].value_in(units.cm)/RSun, color="orange")
+    earth = plt.scatter(gravity_particles.x[0].value_in(units.cm)/RSun, gravity_particles.y[0].value_in(units.cm)/RSun, color="green")
 
-    # # Coordinates for every timestep
+    # Coordinates for every timestep
     # for x, y, lab in zip(list(x_coordinates.values()), list(y_coordinates.values()), labels):
-    #     plt.plot(np.array(x)/au, np.array(y)/au, label=lab)
+        # plt.plot(np.array(x)/au, np.array(y)/au, label=lab)
 
-    plt.xlabel("x [RSun]")
-    plt.ylabel("y [RSun]")
-    plt.legend(loc="upper left")
+# print(min(x_coordinates["earth"])/RSun)
+# print(max(x_coordinates["earth"])/RSun)
+# print(min(y_coordinates["earth"])/RSun)
+# print(max(y_coordinates["earth"])/RSun)
 
-    # plt.xlim(-582, 767)
-    # plt.ylim(-291, 1308)
-    plt.xlim(-660, 675)
-    plt.ylim(-646, 534)
+plt.colorbar(result, label="Age [yr]")
 
-    # axes = plt.gca()
-    # x1, x2 = axes.get_xlim()
-    # xaxis.append(x1)
-    # xaxis.append(x2)
-    # y1, y2 = axes.get_ylim()
-    # yaxis.append(y1)
-    # yaxis.append(y2)
+plt.xlabel("x [RSun]")
+plt.ylabel("y [RSun]")
+plt.legend(handles=[core, gas, earth], labels=label_names, loc="lower left")
 
-    plt.title("Coordinates for t_end={}yr and n={} (i={})".format(t_end, n, i))
-    # plt.show()
-    plt.savefig("animation/scatter_plot_t_end={}yr_n={}_i={}.png".format(t_end, n, i))
-    plt.close()
+plt.xlim(-1024, 1277)
+plt.ylim(-1254, 1161)
+
+# axes = plt.gca()
+# x1, x2 = axes.get_xlim()
+# xaxis.append(x1)
+# xaxis.append(x2)
+# y1, y2 = axes.get_ylim()
+# yaxis.append(y1)
+# yaxis.append(y2)
+
+plt.title("Coordinates for t_end={}yr, n={} and z={}".format(t_end, n, z))
+plt.show()
+# plt.savefig("animation/scatter_plot_t_end={}yr_n={}_i={}_z={}_same_scale.png".format(t_end, n, i, z))
+plt.close()
 
 # print(min(xaxis))
 # print(max(xaxis))
