9fb14e21348d755b19ba2105801f68ab626e6295
amberremmel <54839039+amberremmel@users.noreply.github.com>
2021-12-21 22:55:55 +0100

    Changed the file path

--- sun_visualisation.py
+++ sun_visualisation.py
@@ -1,21 +1,19 @@
-from amuse.lab import *
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import ast
-import sys
 
 # Will only work for the files that come from the new mesa_gravity_hydro.py
 
-t_evolve = 12136
-t_end = 15
-n = 15
-z = 0.0134
+t_evolve = 12136 # Myr
+t_end = 15 # yr
+n = 15 # number of loops
+z = 0.0134 # metallicity
    
 # The csv file is otherwise too big to use
 csv.field_size_limit(sys.maxsize)
 
-with open("t_end={}yr_n={}_z={}/temperature_luminosity_sun_t_evolve={}Myr_z={}.csv".format(t_end, n, z, t_evolve, z)) as csvfile:
+with open("temperature_luminosity_sun_t_evolve={}Myr_z={}.csv".format(t_end, n, z, t_evolve, z)) as csvfile:
     temp_lum = csv.reader(csvfile)
     temperatures = ast.literal_eval(next(temp_lum)[1])
     luminosities = ast.literal_eval(next(temp_lum)[1])
@@ -31,6 +29,6 @@ plt.xlabel("Time [Myr]")
 plt.ylabel(r"M / M$_\odot$")
 # plt.legend(loc="upper left")
 plt.title("Mass of the Sun as a function of time")
-plt.savefig("t_end={}yr_n={}_z={}/mass_t_end={}yr_n={}_z={}.png".format(t_end, n, z, t_end, n, z))
+plt.savefig("mass_t_end={}yr_n={}_z={}.png".format(t_end, n, z, t_end, n, z))
 # plt.show()
 plt.close()
